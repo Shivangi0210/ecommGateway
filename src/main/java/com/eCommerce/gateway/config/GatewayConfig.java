0139f581d97839fa97b9a9226434fcef1f1ba29c
shivangi0210 <shivangi.singh0210@gmail.com>
2021-12-02 11:45:13 +0530

    Updated Routes URLs

--- src/main/java/com/eCommerce/gateway/config/GatewayConfig.java
+++ src/main/java/com/eCommerce/gateway/config/GatewayConfig.java
@@ -1,29 +1,42 @@
 package com.eCommerce.gateway.config;
 
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 
 @Configuration
 public class GatewayConfig {
 
-    @LoadBalanced
+    
     @Bean
     public RouteLocator eCommRouteConfig(RouteLocatorBuilder builder){
         System.out.println("Im in gateway-----------------------------");
         return builder.routes()
-                .route(r -> r.path("/ecomm*","/ecomm/*")
+                .route(r -> r.path("/ecomm/create*","/ecomm/create/*")
                         .filters(f->f.circuitBreaker(c->c.setName("ProviderCB")
-                                                        .setFallbackUri("forward:/provider-failure")
+                                                        .setFallbackUri("forward:/provider-failover/create/")
                                                         .setRouteId("prov-failover")))
                          .uri("lb://eCommerce-order"))
-                .route(r->r.path("/provider-failure/**")
-                         .uri("lb://provider-failure"))
+                .route(r -> r.path("/ecomm/view*","/ecomm/view/*")
+                        .filters(f->f.circuitBreaker(c->c.setName("ProviderCB")
+                                                        .setFallbackUri("forward:/provider-failover/view/")
+                                                        .setRouteId("prov-failover")))
+                         .uri("lb://eCommerce-order"))
+                .route(r -> r.path("/ecomm/update*","/ecomm/update/*")
+                        .filters(f->f.circuitBreaker(c->c.setName("ProviderCB")
+                                                        .setFallbackUri("forward:/provider-failover/update/")
+                                                        .setRouteId("prov-failover")))
+                         .uri("lb://eCommerce-order"))
+                .route(r -> r.path("/ecomm/cancel*","/ecomm/cancel/*")
+                        .filters(f->f.circuitBreaker(c->c.setName("ProviderCB")
+                                                        .setFallbackUri("forward:/provider-failover/cancel/")
+                                                        .setRouteId("prov-failover")))
+                         .uri("lb://eCommerce-order"))
                 .build();
     }
+    
+ 
 }
